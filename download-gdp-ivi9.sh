608c8ccf2f1b9526ceaf90de08d45b3ec4a49fb6
Gianpaolo Macario <gmacario@gmail.com>
2016-02-17 11:51:31 +0100

    Check-in before testing script
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- download-gdp-ivi9.sh
+++ download-gdp-ivi9.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # See https://at.projects.genivi.org/wiki/x/64Kw
 
-set -x
+# set -x
 set -e
 
 REMOTEDIR=http://docs.projects.genivi.org/gdp_files/v9/
 FILELIST=md5sum-gdp-v9beta.txt
 
-while read line < ${FILELIST}; do
-    echo DEBUG: line=$line
-    # TODO
-    # if [ ! -e $f ]; then
-    #    echo INFO: Fetching $f from ${REMOTEDIR}
-    #    wget ${REMOTEDIR}/$f
-    # f
-done
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    # echo DEBUG: line=$line
+    [[ $line = \#* ]] && continue
+    f=$(echo $line | cut -d' ' -f2)
+    echo DEBUG: f=$f
+    if [ ! -e $f ]; then
+        echo INFO: Fetching $f from ${REMOTEDIR}
+        wget ${REMOTEDIR}/$f
+    fi
+done < ${FILELIST}
 md5sum -c ${FILELIST}
 
 # EOF

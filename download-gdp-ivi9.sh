3553871baacf26f430cf287b4ae37ad965639b36
Gianpaolo Macario <gmacario@gmail.com>
2016-02-17 13:05:37 +0100

    Remove debug messages, add info
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- download-gdp-ivi9.sh
+++ download-gdp-ivi9.sh
@@ -4,19 +4,22 @@
 # set -x
 set -e
 
-REMOTEDIR=http://docs.projects.genivi.org/gdp_files/v9/
+REMOTEDIR=http://docs.projects.genivi.org/gdp_files/v9
 FILELIST=md5sum-gdp-v9beta.txt
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     # echo DEBUG: line=$line
     [[ $line = \#* ]] && continue
     f=$(echo $line | cut -d' ' -f2)
-    echo DEBUG: f=$f
-    if [ ! -e $f ]; then
-        echo INFO: Fetching $f from ${REMOTEDIR}
+    # echo DEBUG: f=$f
+    if [ -e $f ]; then
+        echo "INFO: File $f exists - skipping"
+    else
+        echo "INFO: Fetching $f from ${REMOTEDIR}"
         wget ${REMOTEDIR}/$f
     fi
 done < ${FILELIST}
+echo "INFO: Verifying MD5SUMs in ${FILELIST}"
 md5sum -c ${FILELIST}
 
 # EOF
